4035662670353112bf593bd4ad917b845dae97d8
Aman Abhishek <70442464+Aman-Abhishek-18@users.noreply.github.com>
2024-06-27 12:27:31 +0530

    Update Quarterly Sales Profit Question.sql

--- Quarterly Sales Profit Question.sql
+++ Quarterly Sales Profit Question.sql
@@ -7,8 +7,11 @@ SELECT * FROM Orders
 SELECT * FROM Details
 SELECT DATEPART(YEAR, o.Order_Date) as Year, DATEPART(QUARTER, o.Order_Date) AS Quarter, SUM(d.Amount) AS TotalSales,
 SUM(d.Profit) as TotalProfit
-FROM Orders o INNER JOIN Details d ON o.Order_ID = d.Order_ID
-WHERE YEAR(o.Order_Date) = 2018 GROUP BY DATEPART(QUARTER, o.Order_Date), DATEPART(YEAR, o.Order_Date) ORDER BY Quarter
+FROM Orders o 
+INNER JOIN Details d ON o.Order_ID = d.Order_ID
+WHERE YEAR(o.Order_Date) = 2018 
+GROUP BY DATEPART(QUARTER, o.Order_Date), DATEPART(YEAR, o.Order_Date) 
+ORDER BY Quarter
 
 
 
