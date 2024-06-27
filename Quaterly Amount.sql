019ca286a68aa3cae3121db9d0420bbcd8adb2fe
Aman Abhishek <70442464+Aman-Abhishek-18@users.noreply.github.com>
2024-06-27 12:29:01 +0530

    Update Quaterly Amount.sql

--- Quaterly Amount.sql
+++ Quaterly Amount.sql
@@ -1,3 +1,7 @@
+--This SQL query calculates the total sum of payment amounts for each quarter of each year from the payment table. 
+--It groups the data by year and quarter, and orders the results by year and then by quarter.
+
+--(refer PNG file of the same to see the table and result)
 select * from payment
 
 --Quarterly Amount
