956f3520da51e6c936b79a846e9285ea29bc874c
Aman Abhishek <70442464+Aman-Abhishek-18@users.noreply.github.com>
2024-06-27 12:26:54 +0530

    Update Quarterly Sales Profit Question.sql

--- Quarterly Sales Profit Question.sql
+++ Quarterly Sales Profit Question.sql
@@ -1,3 +1,8 @@
+--The SQL script provided consists of three parts: two simple SELECT statements to retrieve all records 
+--from the Orders and Details tables, and a more complex query that calculates quarterly sales and profit for the year 2018.
+
+--(refer PNG file of the same to see the table and result)
+
 SELECT * FROM Orders
 SELECT * FROM Details
 SELECT DATEPART(YEAR, o.Order_Date) as Year, DATEPART(QUARTER, o.Order_Date) AS Quarter, SUM(d.Amount) AS TotalSales,
