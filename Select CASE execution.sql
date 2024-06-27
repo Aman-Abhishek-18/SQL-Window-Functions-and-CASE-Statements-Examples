7d076d7a0755a45b64333942eab9ff94c999bf44
Aman Abhishek <70442464+Aman-Abhishek-18@users.noreply.github.com>
2024-06-27 12:32:07 +0530

    Update Select CASE execution.sql

--- Select CASE execution.sql
+++ Select CASE execution.sql
@@ -1,3 +1,7 @@
+--CASE Statement: The CASE statement is used to create conditional logic within the SQL query. 
+--It evaluates the Profit column and assigns a corresponding value to the Target_Status column 
+--based on the specified conditions.
+
 select *,
 case
 	when Profit < 499 then 'Failure'
@@ -5,3 +9,5 @@ case
 	else 'Success ++'
 end as Target_Status
 from Details
+
+--(refer PNG file of the same to see the table and result)
