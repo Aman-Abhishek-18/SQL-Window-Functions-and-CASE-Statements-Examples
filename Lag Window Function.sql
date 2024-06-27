af6cbd189df6dd72c91bf0de90e525a54ca2f7ed
Aman Abhishek <70442464+Aman-Abhishek-18@users.noreply.github.com>
2024-06-27 12:23:28 +0530

    Update Lag Window Function.sql

--- Lag Window Function.sql
+++ Lag Window Function.sql
@@ -1,3 +1,7 @@
+--The LAG window function in SQL is used to access data from a previous row within the same result set without the 
+--need for a self-join. This function is useful for performing operations that require comparisons between rows, 
+--such as calculating the difference between values in consecutive rows.
+
 
 SELECT 
 DATEPART(MONTH, DateofPayment) as month, sum(Amount) as monthly_sale,
@@ -9,3 +13,5 @@ case
 	else 0
 end as flag
 FROM payment group by DATEPART(MONTH, DateofPayment) order by DATEPART(MONTH, DateofPayment)
+
+--(refer PNG file of the same to see the table and result)
